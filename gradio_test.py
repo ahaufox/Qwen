5d811a4c1156dadb1c999cbf9ca1db2968051945
feijian liu <ahaufox@vip.qq.com>
2023-10-17 10:41:58 +0800

    新增google搜索tool

--- gradio_test.py
+++ gradio_test.py
@@ -12,7 +12,7 @@ os.environ['google_api_key'] = 'AIzaSyAmhsM4aHaontjLQyTKnOITuSITFQ-lptM'
 google_cse_id = '90e04cfa594ec4096'
 wra = GoogleSearchAPIWrapper(google_api_key=google_api_key,
                              google_cse_id=google_cse_id)
-search = GoogleSearchResults(api_wrapper=wra)
+search = GoogleSearchResults(api_wrapper=wra, num_results=10)
 python = PythonAstREPLTool()
 
 
@@ -97,7 +97,7 @@ def build_planning_prompt(TOOLS, query):
     return prompt
 
 
-prompt_1 = build_planning_prompt(TOOLS, query="明天天气怎么样？")
+prompt_1 = build_planning_prompt(TOOLS, query="最近几天发布的关于语言大模型应用相关的新闻？")
 
 # 国内连 hugginface 网络不好，这段代码可能需要多重试
 checkpoint = "Qwen/Qwen-7B-Chat-Int4"
@@ -108,8 +108,9 @@ MODEL.generation_config.do_sample = False  # greedy
 stop = ["Observation:", "Observation:\n"]
 react_stop_words_tokens = [TOKENIZER.encode(stop_) for stop_ in stop]
 response_1, _ = MODEL.chat(TOKENIZER, prompt_1, history=None, stop_words_ids=react_stop_words_tokens)
-print(response_1)
 
+
+# print(response_1)
 
 def parse_latest_plugin_call(text: str) -> Tuple[str, str]:
     i = text.rfind('\nAction:')
@@ -139,23 +140,15 @@ def use_api(tools, response):
         return "没有找到工具"
 
     api_output = used_tool_meta[0]["tool_api"](action_input)
-    print(type(api_output))
+
     # api_output=json.loads(api_output)
     return api_output
 
 
 api_output = use_api(TOOLS, response_1)
-
-print(api_output)
-[{'title': '加拿大人口今天将达到4000万| Radio-Canada.ca',
-  'link': 'https://ici.radio-canada.ca/rci/zh-hans/%E6%96%B0%E9%97%BB/1988668/%E5%8A%A0%E6%8B%BF%E5%A4%A7%E4%BA%BA%E5%8F%A3-%E5%B0%86%E8%BE%BE%E5%88%B04000%E4%B8%87-',
-  'snippet': '2023年6月16日 ... 根据加拿大统计局预测，加拿大人口今天（2023年6月16日）预计将超过4000万。 联邦统计局使用模型来实时估计加拿大的人口，该计数模型预计加拿大人口将\xa0...'},
- {'title': '加拿大制定创纪录移民引入目标：未来三年吸引145万人- 纽约时报 ...',
-  'link': 'https://cn.nytimes.com/world/20221103/canada-immigrants/',
-  'snippet': '2022年11月3日 ... 加拿大移民部长肖恩·弗雷泽周二宣布的新政目标是在2023年至2025年期间吸引145万移民。就在上周，加拿大人口普查机构宣布，现有超过五分之一的加拿大人是\xa0...'},
- {'title': '各国家和地区人口列表- 维基百科，自由的百科全书',
-  'link': 'https://zh.wikipedia.org/zh-hans/%E5%90%84%E5%9B%BD%E5%AE%B6%E5%92%8C%E5%9C%B0%E5%8C%BA%E4%BA%BA%E5%8F%A3%E5%88%97%E8%A1%A8',
-  'snippet': '官方人口普查（页面存档备份，存于互联网档案馆）. 70. 危地马拉, 17,358,000, 2022年7月1日, 0.22%, 官方人口预测. 71. 柬埔寨, 17,091,464, 2023年7月1日, 0.21%, 官方\xa0...'},
- {'title': '加拿大制定創紀錄移民引入目標：未來三年吸引145萬人- 紐約時報 ...',
-  'link': 'https://cn.nytimes.com/world/20221103/canada-immigrants/zh-hant/',
-  'snippet': '2022年11月3日 ... 加拿大移民部長肖恩·弗雷澤週二宣布的新政目標是在2023年至2025年期間吸引145萬移民。就在上週，加拿大人口普查機構宣布，現有超過五分之一的加拿大人是\xa0...'}]
+api_output = eval(api_output)
+# print(api_output)
+for i in api_output:
+    print(i['title'], i['snippet'])
+    print(i['link'])
+    print('\n')
